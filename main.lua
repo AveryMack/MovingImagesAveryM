ad418a6d1ed5c524d1bc316ce07dc8d57b60f302
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-01 10:46:06 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,9 @@
+-- Title: MovingObjects
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program displays a beetleship moving across the screen as well as another 
+-- object moving in a different direction 
+
+print ("***Testing beetleship!")
+
+-- 
